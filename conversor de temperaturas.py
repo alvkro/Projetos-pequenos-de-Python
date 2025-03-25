735b933034cc29ce0fbb094532676ef781dc7043
Álvaro Barbosa Filho <alvaro.filho.111@ufrn.edu.br>
2025-03-24 21:08:53 -0300

    update conversor de temperaturas.py
    Consegui adicionar um verificador de input usando a função while, caso um usuário coloque um comando inválido ele consiga repetir sem que precise sair do programa

--- conversor de temperaturas.py
+++ conversor de temperaturas.py
@@ -27,23 +27,32 @@ print('3- Kelvin\n')
 
 decisao = input(('Digite a opção desejada (1, 2 ou 3): '))
 print('')
+    
+while decisao != '1' and decisao != '2' and decisao != '3':
+    print('Input inválido! Tente novamente.')
+    decisao = input(('Digite a opção desejada (1, 2 ou 3): '))
 
 # Celsius --> any
 
 if decisao == '1':
-    c = float(input('Digite o valor dessa temperatura: \n'))
+    c = float(input('Digite o valor dessa temperatura: '))
     print('Para qual unidade deseja converter?')
     print('1- Fahrenheit')
     print('2- Kelvin\n')
     print('')
     
-    decisao2 = input(('Digite a opção desejada (1 ou 2): \n'))
+    decisao2 = input(('Digite a opção desejada (1 ou 2): '))
     print('')
+
+    while decisao2 != '1' and decisao2 != '1' :
+        print('Input inválido! Tente novamente.')
+        decisao2 = input(('Digite a opção desejada (1, ou 2): '))
 
     if decisao2 == '1':
         print(f'\nA conversão de Celsius para Fahrenheit é {c_f(c)}°F')
-    elif decisao2 == '2':
-        print(f'A conversão de Celsius para Fahrenheit é {c_k(c)}K')
+
+    if decisao2 == '2':
+        print(f'A conversão de Celsius para Kelvin é {c_k(c)}K')
 
 # Fahrenheit --> any
 
@@ -55,12 +64,16 @@ if decisao == '2':
     print('2- Kelvin')
     print('')
     
-    decisao2 = input(('Digite a opção desejada (1 ou 2): \n'))
+    decisao2 = input(('Digite a opção desejada (1 ou 2): '))
+
+    while decisao2 != '1' and decisao2 != '2':
+        print('Input inválido! Tente novamente.')
+        decisao2 = input(('Digite a opção desejada (1, ou 2): '))  
 
     if decisao2 == '1':
         print(f'\nA conversão de Fahrenheit para Celsius é {f_c(f)}°C')
     elif decisao2 == '2':
-        print(f'\nA conversão de Fahrenheit para Kelvin é {f_k(f)}°C')
+        print(f'\nA conversão de Fahrenheit para Kelvin é {f_k(f)}K')
 
 # Kelvin --> any
 
@@ -70,14 +83,13 @@ if decisao == '3':
     print('1- Celsius')
     print('2- Fahrenheit\n')
     
-    decisao2 = input(('Digite a opção desejada (1 ou 2): \n'))
+    decisao2 = input(('Digite a opção desejada (1 ou 2): '))
+
+    while decisao2 != '1' and decisao2 != '2':
+        print('Input inválido! Tente novamente.')
+        decisao2 = input(('Digite a opção desejada (1, ou 2): '))  
 
     if decisao2 == '1':
         print(f'\nA conversão de Kelvin para Celsius é {k_c(k)}°C')
     elif decisao2 == '2':
-        print(f'\nA conversão de Kelvin para Fahrenheit é {k_f(k)}°C')
-
-
-
-
-
+        print(f'\nA conversão de Kelvin para Fahrenheit é {k_f(k)}°F')
